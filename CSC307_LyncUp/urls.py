35842ecc58567287eb8fc9fb50dcdf3b6b798e02
Matthew Jaojoco <mjaojoco@calpoly.edu>
2020-02-23 16:26:09 -0800

    profile pics work

--- CSC307_LyncUp/urls.py
+++ CSC307_LyncUp/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from users import views as user_views
 
 #from django.conf.urls import include, url
-#from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
@@ -27,5 +28,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('LyncUp.urls')),
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
