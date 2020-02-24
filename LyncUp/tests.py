1becdf1693a478e5aa5351d60ec99465cbd472b3
Gabriel Barney <gabriel@Gabriels-MacBook-Pro-4.local>
2020-02-23 16:56:07 -0800

    2/23 deliverables/code coverage

--- LyncUp/tests.py
+++ LyncUp/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.contrib.auth.models import User
+from django.utils import timezone
+import pytz
+
+from .models import Post
+
+
+class TestCases(TestCase):
+    def test_fields(self):
+        user1 = User(username="gabriel",
+                     password="test1",
+                     email="test@test.com")
+        user1.save()
+        post = Post(title="title!", content="content!",
+                     date_posted=timezone.now(),
+                     author=User.objects.first())
+        post.save()
+        tester = Post.objects.first()
+        self.assertEqual(tester, post)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.utils import timezone
+
+# USE MATTS SHIT
+
+class User(models.Model):
+    username = models.CharField(max_length = 20)
+    password = models.TextField()
+    email = models.TextField()
