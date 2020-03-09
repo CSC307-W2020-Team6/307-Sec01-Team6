7373fe64ccb14362172ca745b80b6b23f11e57d4
Matthew Jaojoco <mjaojoco@calpoly.edu>
2020-03-08 19:34:42 -0700

    can't register user

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import UserManager, BaseUserManager
