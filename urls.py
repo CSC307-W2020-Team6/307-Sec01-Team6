17ada9b40780e5dfbad945eb385960a3a1acf8c1
Gabriel Barney <gabriel@Gabriels-MacBook-Pro-4.local>
2020-03-09 11:11:10 -0700

    friends can be removed, gitignore updated

--- urls.py
+++ urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('addfriend/', user_views.add_friends, name='addfriend'),
+    path('removefriend/', user_views.remove_friends, name='removefriend'),
     #url(r'^connect/(?P<operation>.+)/(?P<pk>\d+)/$', user_views.add_friends, name='add_friends'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
