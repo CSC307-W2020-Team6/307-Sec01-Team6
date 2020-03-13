9536454e52b68b73010a69ac18eff7619f1e903a
Matthew Jaojoco <mjaojoco@calpoly.edu>
2020-03-13 01:40:47 -0700

    home button reassigned

--- LyncUp/urls.py
+++ LyncUp/urls.py
@@ -5,6 +5,7 @@ from LyncUp import views as LyncUp_views
 
 
 urlpatterns = [
+    path('', views.home, name='LyncUp-home'),
     path('group/list/', GroupListView.as_view(), name='group-list'),
     # path with variable
     path('group/<int:pk>/update/', GroupUpdateView.as_view(), name='group-update'),
